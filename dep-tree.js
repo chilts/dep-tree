828f9f27988a089da73f68058fce32dd9589fca6
Andrew Chilton <andychilton@gmail.com>
2013-09-10 16:12:12 +1200

    Make nicer

--- dep-tree.js
+++ dep-tree.js
@@ -18,24 +18,20 @@ DepTree.prototype.add = function(parent, child) {
 
 DepTree.prototype.solve = function(child) {
     var self = this;
-    var parents = [];
 
     // if we don't know about this child, return nothing
     if ( !self.parentsOf[child] ) {
         return [];
     }
 
-    parents = [ child ];
-
     // loop through all of the parents of this child (backwards to keep order) and add them on
+    var parents = [ child ];
     self.parentsOf[child].reverse().forEach(function(parent) {
         parents.unshift(parent);
         parents.unshift(self.solve(parent));
     });
 
-    parents = _.flatten(parents);
-
-    return _.uniq(parents);
+    return _.chain(parents).flatten().uniq().value()
 };
 
 module.exports = DepTree;
